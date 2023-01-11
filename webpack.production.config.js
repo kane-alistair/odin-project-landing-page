4c3c265c9ebb162ca4c5e90c7e5176e113a91188
AlistairKaneAND <alistair.kane@and.digital>
2023-01-11 14:38:39 +0000

    setup github-pages

---
+++ webpack.production.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+const path = require('path')
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+    },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
